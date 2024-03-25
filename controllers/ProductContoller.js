72dd80c60abc46eb901738682528988d98640070
nouman26 <noumanarain0@gmail.com>
2024-03-25 13:12:16 +0500

    Updated

--- controllers/ProductContoller.js
+++ controllers/ProductContoller.js
@@ -19,7 +19,7 @@ const storage = multer.diskStorage({
 // Multer upload limits
 const upload = multer({
   storage: storage,
-  limits: { fileSize: 5 * 1024 * 1024 }, // Limit file size to 5MB
+  // limits: { fileSize: 5 * 1024 * 1024 }, // Limit file size to 5MB
   fileFilter: function (req, file, cb) {
     // Only allow images
     if (file.mimetype.startsWith('image/')) {
